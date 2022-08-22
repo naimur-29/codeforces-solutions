02638271abdefd7e89fd4d71a9cc84fd2af7daa9
Naim <skylike2904@gmail.com>
2022-08-22 21:29:43 +0600

    up

--- 800/151A-o-.cpp
+++ 800/151A-o-.cpp
@@ -3,7 +3,8 @@
 // solved
 using namespace std;
 
-int main() {
+int main()
+{
     int n, k, l, c, d, p, nl, np;
     int res{0};
 
--- 800/490A.cpp
+++ 800/490A.cpp
@@ -4,36 +4,50 @@
 // ongoing...
 using namespace std;
 
-int main() {
-    int limit{0}, res{0}, num, count1{0}, count2{0}, count3{0};
+int main()
+{
+    int limit{}, res{}, num, count1{}, count2{}, count3{};
 
     cin >> limit;
 
-    int res_arr[5000/3][3];
-    int ind_arr[3];
+    int res_arr[2000][3] {}, ind[3] {};
 
-    for (int i = 1; i <= limit; i++) {
+    for (int i = 1; i <= limit; i++)
+    {
         cin >> num;
 
         count1 += num == 1 ? 1 : 0;
-        ind_arr[0] = num == 1 ? i : ind_arr[0];
         count2 += num == 2 ? 1 : 0;
-        ind_arr[1] = num == 2 ? i : ind_arr[1];
         count3 += num == 3 ? 1 : 0;
-        ind_arr[2] = num == 3 ? i : ind_arr[2];
 
-        if (count1 && count2 && count3) {
-            res += 1;            
-            res_arr[res-1][0] = ind_arr[0];
-            res_arr[res-1][1] = ind_arr[1];
-            res_arr[res-1][2] = ind_arr[2];
+        for (int j = 0; j < 3; j++)
+        {
+            if (num == j+1)
+            {
+                if (!res_arr[ind[j]][j])
+                {
+                    res_arr[ind[j]][j] = i;
+                }
+                else
+                {
+                    ind[j]++;
+                    res_arr[ind[j]][j] = i;
+                }
+            }
+        }
+
+        if (count1 && count2 && count3)
+        {
+            res += 1;
             count1--; count2--; count3--;
         }
     }
 
     cout << res << endl;
-    for (int i = 0; i < res; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < res; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cout << res_arr[i][j] << ' ';
         }
         cout << endl;
