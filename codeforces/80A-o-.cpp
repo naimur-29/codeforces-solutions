04cf3cdfda4878f6546424747febd53a1623026a
Naim <skylike2904@gmail.com>
2022-10-25 22:28:45 +0600

    up

---
+++ codeforces/80A-o-.cpp
@@ -0,0 +1,51 @@
+/*
+⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀
+⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀ ⡏⠀⠀⠀⠀⠀⢷
+⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇
+⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀9ain ⡇
+⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇
+⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼
+⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀
+⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀
+⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀
+⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀
+⣿⣿⣧⣀⣿.........⣀⣰⣏⣘⣆⣀
+*/
+
+#include <iostream>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    bool isNextPrime;
+
+    int multiples{};
+    for (int j = 2; j <= (m/2)+1; j++) {
+        if (m % j == 0)
+            multiples++;
+    }
+    isNextPrime = !multiples;
+
+    if (isNextPrime) {
+        for (int i = n+1; i < m; i++) {
+            multiples = 0;
+            for (int j = 2; j <= (i/2)+1; j++) {
+                if (i % j == 0)
+                    multiples++;
+            }
+
+            if (!multiples) {
+                isNextPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isNextPrime)
+        std::cout << "YES" << std::endl;
+    else
+        std::cout << "NO" << std::endl;
+
+    return 0;
+}
